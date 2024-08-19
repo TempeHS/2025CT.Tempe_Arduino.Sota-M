7eee4b95cda755059637f5b2adaabebc9886966d
sotama <sota.ma@education.nsw.gov.au>
2024-08-19 13:43:30 +1000

    Update 02.storingData.ino

--- 02.storingData/02.storingData.ino
+++ 02.storingData/02.storingData.ino
@@ -10,7 +10,7 @@
     2.  I know how to access more information about the different datatypes in the documentation
     3.  I Understand how (1, HIGH and true) and (0, LOW, false) all represent the same concept and can be
         used interchangeably in an INO sketch
-    4.  I can convert an integer, float and boolean to a string and print it in to the serial monitor
+  
     5.  I understand the difference between an int and an unsigned int or a long or an unsigned long
     6.  I understand the difference between a global variable and a function variable 
 
