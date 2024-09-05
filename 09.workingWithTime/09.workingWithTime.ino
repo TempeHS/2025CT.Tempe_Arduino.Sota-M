2628b4d85f378be0e74a97f7c7d32a44a505ca0f
sotama <sota.ma@education.nsw.gov.au>
2024-09-05 10:35:51 +1000

    Update 09.workingWithTime.ino

--- 09.workingWithTime/09.workingWithTime.ino
+++ 09.workingWithTime/09.workingWithTime.ino
@@ -17,6 +17,19 @@
     https://github.com/TempeHS/TempeHS_Ardunio_Bootcamp/blob/main/09.workingWithTime/Bootcamp-workingWithTime.png
 */
 
+static unsigned int blueLED = 6;
+static unsigned int greenLED = 2;
+unsigned int blueLEDState = LOW;
+unsigned int greenLEDState = LOW;
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long blueLEDPreviousMillis = 0;
+unsigned long greenLEDPreviousMillis = 0;
+
+// Use a long so you can mathematically compare with the variable previousMillis without conversion
+const unsigned long blueLEDinterval = 2000;
+const unsigned long greenLEDinterval = 9000;
 
 void setup() {
   
